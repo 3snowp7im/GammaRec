927350e531d99ef7db6a6a1ad1789513bfdfda73
Wild Mouse <3snowp7im@gmail.com>
2020-07-05 01:47:17 -0700

    1.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'GammaRec',
-    version = '1.0.4',
+    version = '1.0.5',
     description = 'Video capture recording and preview.',
     url = 'http://github.com/3snow_p7im/GammaRec',
     author = 'Wild Mouse',
